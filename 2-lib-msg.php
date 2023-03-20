a0533a317c384b60110bd42215aefc5f3f66690e
GeneralBorN <bornabobohorina@gmail.com>
2023-03-20 18:47:05 +0100

    Update 2-lib-msg.php

--- 2-lib-msg.php
+++ 2-lib-msg.php
@@ -72,7 +72,7 @@ class Message {
 
 // (G) DATABASE SETTINGS - CHANGE TO YOUR OWN!
 define("DB_HOST", "localhost");
-define("DB_NAME", "testni");
+define("DB_NAME", "dtb_student_chatbox");
 define("DB_CHARSET", "utf8mb4");
 define("DB_USER", "root");
 define("DB_PASSWORD", "");
